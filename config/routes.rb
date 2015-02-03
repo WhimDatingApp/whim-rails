1268ceff861d367c328aac80da61d2692f77a143
Bill Palmer <darktriad@users.noreply.github.com>
2015-02-02 21:54:07 -0800

    Stubbed out User controller.  This completes the last of the routes from the Go server.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  # TODO: do we actually need this route or anything potentially under it?
+  # /test/media => serves files under /test/media (???)
+
   namespace :api do
 
     # ----- DATES ----------------------------------------------------------------------------------
@@ -21,6 +24,29 @@ Rails.application.routes.draw do
     # ----- SCHOOLS --------------------------------------------------------------------------------
 
     get "/school/search" => "school#search"
+
+    # ----- USERS ----------------------------------------------------------------------------------
+
+    # TODO: figure out regex constraints for :name and :token
+    get  "/user/attrs"                => "user#attributes"
+    get  "/user/confirm_email/:token" => "user#confirm_email" #, constraints: { token: /.../ }
+    # TODO: verify iOS does not request /content with trailing slash
+    post "/user/content"              => "user#content"
+    post "/user/dates"                => "user#dates"
+    post "/user/destroyuser"          => "user#destroy_user"
+    post "/user/lastplaceid"          => "user#last_place_id"
+    post "/user/loc"                  => "user#location"
+    post "/user/login"                => "user#login"
+    post "/user/logout"               => "user#logout"
+    get  "/user/places"               => "user#places"
+    post "/user/profile"              => "user#profile"
+    post "/user/save_city"            => "user#save_city"
+    post "/user/schedule"             => "user#schedule"
+    post "/user/schedule/set"         => "user#set_schedule"
+    post "/user/sendInvites"          => "user#send_invites"
+    get  "/user/:name/profile"        => "user#user_profile" #, constraints: { name: /.../ }
+    post "/user/verify"               => "user#verify"
+    post "/user/update"               => "user#update"
 
     # ----- VENUES ---------------------------------------------------------------------------------
 
